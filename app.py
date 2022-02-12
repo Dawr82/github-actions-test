82f1a9dba8171f2c43ebbc38fe3d01571466fb06
Dawr82 <gerfo776@gmail.com>
2022-02-12 20:01:38 +0100

    Added REST API endpoint in flask application.

--- app.py
+++ app.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def homepage():
     return "<h1>Welcome to my website<\h1>"
 
+@app.route("/data")
+def api():
+    return {
+        "Name": "Dawid",
+        "Surname": "Skorupa",
+    }
+
 if __name__ == "__main__":
     app.run()
